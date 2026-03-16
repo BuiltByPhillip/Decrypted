27bd6be34c07c1f89f84865561b087ce19f0cbbc
Phillip <phir@itu.dk>
2026-03-16 12:00:22 +0100

    DefinitionContainer.tsx done for now

--- src/app/_components/definition/DefinitionContainer.tsx
+++ src/app/_components/definition/DefinitionContainer.tsx
@@ -18,8 +18,8 @@ export default function DefinitionContainer({ selected, className }: DefinitionC
     if (selected.size === 0) return null;
 
     return (
-        <div className={`min-w-40 min-h-60 border-2 border-muted rounded-2xl p-4 ${className}`}>
-            <span className="font-bold text-xl text-muted select-none">Your Definitions</span>
+        <div className={`min-w-40 min-h-60 border-2 border-muted rounded-2xl p-4 bg-muted/10 ${className}`}>
+            <span className="font-bold text-xl text-muted select-none">Definitions</span>
             <div className={`grid grid-cols-[2fr_1fr_auto] pt-2 text-muted`}>
                 {convertToList().map(([role, expr]) => (
                     <React.Fragment key={role}>
