ff2f68d389c0b65e581af7390a440586b221b061
Phillip <phir@itu.dk>
2026-03-18 13:26:19 +0100

    Still building the CalculateExercise.tsx

--- src/app/_components/exercises/calculate/CalculateExercise.tsx
+++ src/app/_components/exercises/calculate/CalculateExercise.tsx
@@ -3,6 +3,7 @@ import Input from "~/app/_components/exercises/shared/Input";
 import type {SelectedDefinitions} from "~/app/exercise/page";
 import type {Expr} from "~/app/hooks/parser";
 import {useState} from "react";
+import Hint from "~/app/_components/exercises/select/hint";
 
 type CalculateExerciseProps = {
     description: string;
@@ -21,9 +22,13 @@ export default function CalculateExercise({ description, prompt, hint, answer, o
     }
     
     return (
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center items-center">
             <ExerciseDescription description={description} prompt={prompt} definitions={definitions}/>
-            <Input placeholder="Enter your answer"/>
+            <div className="flex flex-col items-center">
+                <Hint hint={hint ?? null} className="self-end"/>
+                <Input placeholder="Enter your answer"/>
+            </div>
+            
         </div>
     );
 }
--- src/app/_components/exercises/select/hint.tsx
+++ src/app/_components/exercises/select/hint.tsx
@@ -2,16 +2,17 @@ import Button from "~/components/Button";
 import { CircleQuestionMark } from "lucide-react";
 
 type HintProps = {
-  hint: string;
+  hint: string | null;
+  className?: string;
 }
 
-export default function Hint({hint}: HintProps) {
+export default function Hint({ hint, className}: HintProps) {
 
   return (
-    <div className="flex items-center">
-      <Button variant="ghost" className="flex items-center px-2">
+    <div className={`flex items-center ${className}`}>
+      <Button variant="ghostMuted" className="flex items-center px-2">
         <CircleQuestionMark size={25} />
-        <span className="pl-1">Hint</span>
+        <span className="pl-1">Help</span>
       </Button>
     </div>
 
