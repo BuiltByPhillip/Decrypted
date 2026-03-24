a23ce17dbcb59b647d241238a3c5e4e4183b7c13
Phillip <phir@itu.dk>
2026-03-24 13:35:43 +0100

    Created ExerciseShell.tsx

--- src/app/_components/exercises/shared/ExerciseShell.tsx
+++ src/app/_components/exercises/shared/ExerciseShell.tsx
@@ -1,6 +1,8 @@
 import type { SelectedDefinitions } from "~/app/exercise/page";
-import React from "react";
+import React, { useState } from "react";
 import ExerciseDescription from "~/app/_components/exercises/shared/ExerciseDescription";
+import Button from "~/components/Button";
+import Hint from "~/app/_components/exercises/select/hint";
 
 type ExerciseShellProps = {
   description: string;
@@ -15,11 +17,37 @@ type ExerciseShellProps = {
 
 
 export default function ExerciseShell({ description, prompt, hint, onSubmit, submitState, children, definitions, feedback }: ExerciseShellProps) {
+  const [showHint, setShowHint] = useState(false);
 
   return (
     <div>
-      <ExerciseDescription/>
+      {/* Description & prompt */}
+      <ExerciseDescription
+        description={description}
+        prompt={prompt}
+        definitions={definitions}
+      />
 
+      {children}
+
+      <div className="flex flex-col items-center pt-10">
+        <div className="relative flex w-150 justify-center items-center">
+          <div className="absolute bottom-full w-full flex flex-col items-center pb-2">
+            {showHint && hint
+              ? <span className="text-muted text-sm">{hint}</span>
+              : feedback
+            }
+          </div>
+          <Button variant="submit" className="w-100" onClick={onSubmit}>
+            {submitState === "correct" ? "Correct!" : submitState === "incorrect" ? "Incorrect!" : "Check answer"}
+          </Button>
+          {hint && (
+            <div className="absolute right-0">
+              <Hint open={showHint} onClick={() => setShowHint(s => !s)} />
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
